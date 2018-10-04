d90e861f94c886016d472f8138970551e1e7b173
Caleb Smith <karagenit@outlook.com>
2018-10-04 13:14:31 -0400

    Use Maven Deploy over Install

--- install.sh
+++ install.sh
@@ -3,14 +3,15 @@
 read -p "Group: " groupId
 read -p "Artifact: " artifactId
 read -p "Version: " version
-read -p "File: " filePath
-read -p "Sources: " sourcePath
-read -p "Javadoc: " javadocPath
+read -e -p "File: " filePath
+read -e -p "Sources: " sourcePath
+read -e -p "Javadoc: " javadocPath
 
 filePath=$(realpath $filePath)
 repoPath=$(realpath ./maven)
+repoPath="file://${repoPath}"
 
-mvn install:install-file \
+mvn deploy:deploy-file \
     -DgroupId=$groupId -DartifactId=$artifactId -Dversion=$version \
     -Dfile=$filePath -Dpackaging=jar -DgeneratePom=true \
-    -DlocalRepositoryPath=$repoPath -DcreateChecksum=true
+    -Durl=$repoPath -DcreateChecksum=true
